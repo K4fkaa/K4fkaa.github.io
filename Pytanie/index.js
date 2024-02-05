a1bff510a78026aba95bee6187acca6f3e4fb870
K4fkaa <72510852+K4fkaa@users.noreply.github.com>
2024-02-05 22:29:15 +0100

    Add files via upload

---
+++ Pytanie/index.js
@@ -0,0 +1,10 @@
+function noFunction(){
+    document.getElementById("box").style.display = "none";
+    document.getElementById("gif2").style.display = "block";
+}
+
+function yesFunction(){
+    document.getElementById("box").style.display = "none";
+    document.getElementById("gif3").style.display = "block";
+    document.getElementById("napis").style.display = "block";
+}
